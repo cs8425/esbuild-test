2753643dd3de422af75df0cd91f778e77d80a4d5
cs8425 <cs8425@gmail.com>
2021-08-30 13:46:46 +0800

    switch to esbuild.build()

--- esbuild-dev.js
+++ esbuild-dev.js
@@ -1,34 +1,21 @@
+const http = require('http');
+const https = require('https');
 const esbuild = require('esbuild');
+const hanlder = require('serve-handler');
+const comm = require('./esbuild-comm.js');
 
 const config = {
 	devPort: 8000,
+	skipGz: true,
 };
 
-let reactOnResolvePlugin = {
-	name: 'react2preact',
-	setup(build) {
-		// let path = require('path')
-
-		// Redirect all 'react' to "./public/images/"
-		build.onResolve({ filter: /^react$/ }, args => {
-			return {
-				path: require.resolve('preact/compat'),
-				external: false,
-			}
-		});
-		build.onResolve({ filter: /^react-dom$/ }, args => {
-			return {
-				path: require.resolve('preact/compat'),
-				external: false,
-			}
-		});
-	},
-}
-
-esbuild.serve({
-	servedir: 'public',
-	port: config.devPort,
-}, {
+// const fileHanlder = new statik.Server('./dist');
+const fileHanlder = (req, res) => hanlder(req, res, {
+	"public": './dist',
+	"etag": true
+});
+esbuild.build({
+	define: { 'process.env.NODE_ENV': '"development"' }, // production
 	entryPoints: ['src/main.js'],
 	bundle: true,
 	// minify: true,
@@ -39,7 +26,8 @@ esbuild.serve({
 		// 'es2015',
 		'es2020',
 	],
-	outfile: 'public/bundle.js',
+	// outfile: 'dist/main.js',
+	outdir: './dist',
 	loader: {
 		'.js': 'jsx',
 		'.png': 'file',
@@ -53,14 +41,34 @@ esbuild.serve({
 	jsxFactory: 'h',
 	jsxFragment: 'Fragment',
 	plugins: [
-		reactOnResolvePlugin,
+		comm.reactOnResolvePlugin,
+		comm.copyPlugin,
 	],
-}).then(async (server) => {
-	console.log('[dev]server start at', server.host, server.port);
-	await server.wait;
-	// Call "stop" on the web server when you're done
-	// server.stop()
-}).catch((err) => {
-	console.log('[dev]err', err);
-	process.exit(1)
+	watch: {
+		onRebuild(error, result) {
+			if (error) {
+				console.error('watch build failed:', error);
+			} else {
+				console.log('watch build succeeded:', result);
+			}
+		},
+	},
+}).then((result) => {
+	console.log('build OK:', result);
+
+	// Then start a proxy server on port config.devPort
+	http.createServer((req, res) => {
+		console.log('[req]', req.url, req.headers);
+
+		if (config.skipGz) delete req.headers['accept-encoding']; // skip gz
+
+		// Serve files!
+		req.addListener('end', function () {
+			// fileHanlder.serve(req, res);
+			fileHanlder(req, res);
+		}).resume();
+		// fileHanlder(req, res);
+
+	}).listen(config.devPort);
 });
+
